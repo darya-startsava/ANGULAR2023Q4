3d04ad2c76d4af7991d5a93f76b128c2252ebbbc
darya-startsava <daryastartsava@gmail.com>
2023-10-31 19:13:35 +0100

    feat: add logo and settings button

--- youtube-client-app/src/app/header/header.component.ts
+++ youtube-client-app/src/app/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 
 import { CustomButtonComponent } from "../custom-button/custom-button.component";
 
 @Component({
     standalone: true,
-    imports: [CustomButtonComponent, MatInputModule, MatFormFieldModule],
+    imports: [
+        CustomButtonComponent,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatIconModule
+    ],
     selector: "app-header",
     templateUrl: "./header.component.html",
     styleUrls: ["./header.component.scss"]
