212c8d12dbe1eaeb1a5d24a5a2cd5f475457a8e4
darya-startsava <daryastartsava@gmail.com>
2023-11-20 00:22:21 +0100

    refactor: change the project structure

--- youtube-client-app/src/app/shared/shared.module.ts
+++ youtube-client-app/src/app/shared/shared.module.ts
@@ -4,7 +4,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 
-import { CustomButtonComponent } from "./custom-button/custom-button.component";
+import { CustomButtonComponent } from "./components/custom-button/custom-button.component";
 import { FilterPipe } from "./pipes/filter.pipe";
 
 @NgModule({
