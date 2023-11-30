c46316e9f26356a2253b8423a09c5c6909de549c
darya-startsava <daryastartsava@gmail.com>
2023-11-30 21:31:44 +0100

    feat: add observable to follow is user logged or not

--- youtube-client-app/src/app/auth/services/login.service.ts
+++ youtube-client-app/src/app/auth/services/login.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
     providedIn: "root"
 })
 export class LoginService {
+    public isLoggedIn$ = new BehaviorSubject<boolean>(false);
+
+    constructor() {
+        this.isLoggedIn$.next(this.checkInitialLoginState());
+    }
+
+    private checkInitialLoginState(): boolean {
+        return !!localStorage.getItem("token");
+    }
     login(): void {
         localStorage.setItem("token", "token");
+        this.isLoggedIn$.next(true);
     }
 
     logout(): void {
         localStorage.removeItem("token");
+        this.isLoggedIn$.next(false);
     }
 }
