1f6defc38349db7428032f822adaf04d7829df18
darya-startsava <daryastartsava@gmail.com>
2023-12-12 01:54:39 +0100

    refactor: remove duplicated code

--- youtube-client-app/src/app/favorite/favorite.module.ts
+++ youtube-client-app/src/app/favorite/favorite.module.ts
@@ -5,15 +5,9 @@ import { provideRouter, RouterLink } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
 import { routes } from "./favorite.routes";
 import { FavoritePageComponent } from "./pages/favorite-page/favorite-page.component";
-import { FavoriteItemsComponent } from "./components/favorite-items/favorite-items.component";
-import { FavoriteItemComponent } from "./components/favorite-item/favorite-item.component";
 
 @NgModule({
-    declarations: [
-        FavoritePageComponent,
-        FavoriteItemsComponent,
-        FavoriteItemComponent
-    ],
+    declarations: [FavoritePageComponent],
     imports: [CommonModule, SharedModule, RouterLink],
     providers: [provideRouter(routes)]
 })
--- youtube-client-app/src/app/youtube/youtube.module.ts
+++ youtube-client-app/src/app/youtube/youtube.module.ts
@@ -1,10 +1,8 @@
 import { CommonModule, DatePipe } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { provideRouter, RouterLink } from "@angular/router";
+import { provideRouter } from "@angular/router";
 
 import { SharedModule } from "../shared/shared.module";
-import { SearchItemComponent } from "./components/search-item/search-item.component";
-import { SearchResultsComponent } from "./components/search-results/search-results.component";
 import { DetailedInformationPageComponent } from "./pages/detailed-information-page/detailed-information-page.component";
 import { MainPageComponent } from "./pages/main-page/main-page.component";
 import { routes } from "./youtube.routes";
@@ -12,11 +10,9 @@ import { routes } from "./youtube.routes";
 @NgModule({
     declarations: [
         MainPageComponent,
-        SearchItemComponent,
-        SearchResultsComponent,
         DetailedInformationPageComponent
     ],
-    imports: [CommonModule, SharedModule, RouterLink, DatePipe],
+    imports: [CommonModule, SharedModule, DatePipe],
     providers: [provideRouter(routes)]
 })
 export class YoutubeModule {}
